be08add51548e67f1a2de692dc8ef174e8894bb1
Manting-Mu <sp44@sciprog.aws1.ichec.ie>
2021-11-16 22:14:09 +0000

    added scripts and read me file for week 9 's practical

---
+++ practical109/magic_square.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"magic_square.h"
+
+int main(){
+
+	int n;
+	printf("Enter the square side:\n");
+	scanf("%d", &n);
+
+	// Allocate a matrix
+	
+	int i;
+	int ** magicSquare = malloc(n * sizeof(int));
+
+	for (i = 0; i<n; i++){
+		magicSquare[i] = malloc(n * sizeof(int));
+	}
+
+	int j;
+
+	for (i = 0; i<n; i++){
+		printf("Enter the elements in row #%d, separated by blacks and/or linebreaks:\n", i+1);
+		for(j= 0; j<n; j++){
+			scanf("%d", &magicSquare[i][j]);
+		}
+	}
+	
+	printf("The square %s magic.\n", isMagicSquare(magicSquare, n)? "is" : "is NOT");
+
+	for(i = 0; i < n; i++) {
+		free(magicSquare[i]);
+	}
+	free(magicSquare);
+
+	return 0;
+}
---
+++ practical109/magic_square_readfile.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_FILE_NAME 100
+#include"magic_square.h"
+
+int getlines(char filenema[MAX_FILE_NAME]);
+
+int main(){
+
+	FILE *f; 
+	char filename[MAX_FILE_NAME];
+	printf("Enter file name: ");
+	scanf("%s", filename);
+
+	f = fopen(filename, "r");
+
+	int n = getlines(filename);
+
+	int i;
+	int ** magicSquare = malloc(n * sizeof(int));
+
+	for (i = 0; i<n; i++){
+		magicSquare[i] = malloc(n * sizeof(int));
+	}
+
+	int j;
+
+	for (i = 0; i<n; i++){
+		
+		for(j= 0; j<n; j++){
+			fscanf(f, "%d", &magicSquare[i][j]);
+		}
+	}
+	
+	printf("The square %s magic.\n", isMagicSquare(magicSquare, n)? "is" : "is NOT");
+
+	for(i = 0; i < n; i++) {
+		free(magicSquare[i]);
+	}
+	free(magicSquare);
+	
+	fclose(f);	
+
+	return 0;
+}
+
+int getlines(char filename[MAX_FILE_NAME]) {
+	
+	FILE *fp;
+	fp = fopen(filename, "r");
+
+	int ch_read;
+	int count = 0;
+
+	while( (ch_read = fgetc(fp)) != EOF)
+	{
+		if (ch_read == '\n'){
+			count++;
+		}
+	}
+	
+	printf("No. of lines %d\n", count);
+	fclose(fp);
+	return count;
+}
